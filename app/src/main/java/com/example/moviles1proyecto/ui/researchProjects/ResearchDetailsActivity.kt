5f263cf4a11594d06fd4783fc894b04f5e194da2
Remma27 <emmanuelrsolano27@gmail.com>
2023-12-10 15:06:16 -0600

    add comments and theme

--- app/src/main/java/com/example/moviles1proyecto/ui/researchProjects/ResearchDetailsActivity.kt
+++ app/src/main/java/com/example/moviles1proyecto/ui/researchProjects/ResearchDetailsActivity.kt
@@ -1,11 +1,8 @@
 package com.example.moviles1proyecto.ui.researchProjects
 
-import android.content.Context
 import android.os.Bundle
-import android.util.TypedValue
 import android.widget.Button
 import android.widget.EditText
-import android.widget.LinearLayout
 import android.widget.RatingBar
 import android.widget.TextView
 import android.widget.Toast
@@ -16,15 +13,14 @@ import com.example.moviles1proyecto.R
 import com.example.moviles1proyecto.ui.comments.Comments
 import com.example.moviles1proyecto.ui.comments.MyAdapter
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
-import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
-import org.w3c.dom.Comment
-import java.util.Date
 
 class ResearchDetailsActivity : AppCompatActivity() {
 
@@ -45,7 +41,6 @@ class ResearchDetailsActivity : AppCompatActivity() {
         ratingBar = findViewById<RatingBar>(R.id.ratingBar)
 
         var db = FirebaseFirestore.getInstance()
-        val commentsContainer = findViewById<LinearLayout>(R.id.commentsContainer)
         val projectID = intent.getStringExtra("PROJECTID")
         val researchTitle = intent.getStringExtra("RESEARCH_TITLE")
         val areaOfInterest = intent.getStringExtra("AREA_OF_INTEREST")
@@ -78,15 +73,77 @@ class ResearchDetailsActivity : AppCompatActivity() {
             finish()
         }
 
+
+        //Hacer un nuevo comentario
         buttonPublish.setOnClickListener {
             val firestore = FirebaseFirestore.getInstance()
             val commentText = commentInput.text.toString()
             val ratingValue = ratingBar.rating
+
             if (commentText.isNotEmpty()) {
                 try {
-                    publishComment(firestore, commentText, ratingValue, commentsContainer)
+                    val currentUser = FirebaseAuth.getInstance().currentUser
+
+                    if (currentUser != null) {
+                        val commentData = hashMapOf(
+                            "commentText" to commentText,
+                            "rating" to ratingValue.toDouble(),
+                            "timestamp" to FieldValue.serverTimestamp(),
+                            "projectID" to projectID,
+                            "uid" to currentUser.uid
+                        )
+
+                        // Verifica si el usuario ha comentado previamente en esta investigación
+                        db.collection("comments")
+                            .whereEqualTo("uid", currentUser.uid)
+                            .whereEqualTo("projectID", projectID)
+                            .get()
+                            .addOnSuccessListener { querySnapshot ->
+                                if (querySnapshot.isEmpty) {
+                                    // Guarda el comentario solo si el usuario no ha comentado en esta investigación antes
+                                    db.collection("comments")
+                                        .add(commentData)
+                                        .addOnSuccessListener {
+                                            Toast.makeText(
+                                                this,
+                                                "Comentario publicado con éxito",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                            commentInput.text.clear()
+                                            ratingBar.rating = 0f
+                                            recreate()
+                                        }
+                                        .addOnFailureListener { e ->
+                                            Toast.makeText(
+                                                this,
+                                                "Error al publicar el comentario: $e",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
+                                } else {
+                                    Toast.makeText(
+                                        this,
+                                        "Ya has comentado en esta investigación.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                            .addOnFailureListener { e ->
+                                Toast.makeText(
+                                    this,
+                                    "Error al verificar comentarios anteriores: $e",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "Por favor, inicia sesión para dejar un comentario.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 } catch (e: Exception) {
-                    handleCommentPublishException(e)
+                    Toast.makeText(this, "Error al publicar el comentario.", Toast.LENGTH_SHORT).show()
                 }
             } else {
                 Toast.makeText(this, "Por favor, escribe un comentario.", Toast.LENGTH_SHORT).show()
@@ -94,7 +151,8 @@ class ResearchDetailsActivity : AppCompatActivity() {
         }
 
 
-        //Comentarios
+
+        //Ver Comentarios
         recyclerView = findViewById(R.id.recyclerviewComments)
         recyclerView.layoutManager = LinearLayoutManager(this)
         commentList = arrayListOf()
@@ -102,18 +160,18 @@ class ResearchDetailsActivity : AppCompatActivity() {
         db.collection("comments").whereEqualTo("projectID", projectID)
             .get()
             .addOnSuccessListener {
-            if (!it.isEmpty){
-                for (data in it.documents){
-                    val comment:Comments? = data.toObject(Comments::class.java)
-                    if (comment != null) {
-                        commentList.add(comment)
+                if (!it.isEmpty) {
+                    for (data in it.documents) {
+                        val comment: Comments? = data.toObject(Comments::class.java)
+                        if (comment != null) {
+                            commentList.add(comment)
+                        }
                     }
+                    recyclerView.adapter = MyAdapter(commentList)
                 }
-                recyclerView.adapter = MyAdapter(commentList)
             }
-        }
-            .addOnFailureListener{
-                Toast.makeText(this,it.toString(),Toast.LENGTH_SHORT).show()
+            .addOnFailureListener {
+                Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
             }
     }
 
@@ -139,73 +197,6 @@ class ResearchDetailsActivity : AppCompatActivity() {
         }
     }
 
-    private fun publishComment(
-        firestore: FirebaseFirestore,
-        commentText: String,
-        ratingValue: Float,
-        commentsContainer: LinearLayout
-    ) {
-        val dt: Date = Date()
-        val commentData = hashMapOf(
-            "commentText" to commentText,
-            "ratingValue" to ratingValue,
-            "timestamp" to dt.toString()
-        )
-
-        firestore.collection("comments")
-            .add(commentData)
-            .addOnSuccessListener { documentReference ->
-                // Register the data into the local storage
-                val prefe = this.getSharedPreferences("appData", MODE_PRIVATE)
-                val editor = prefe.edit()
-
-                editor.putString("commentText", commentText)
-                editor.putString("ratingValue", ratingValue.toString())
-                editor.commit()
-                Toast.makeText(this, "Comentario publicado.", Toast.LENGTH_SHORT).show()
-            }
-            .addOnFailureListener { e ->
-                handleCommentPublishException(e)
-            }
-
-        val newCommentLayout = LinearLayout(this).apply {
-            orientation = LinearLayout.VERTICAL
-            layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            ).apply {
-                bottomMargin = 8.dpToPx(this@ResearchDetailsActivity)
-            }
-        }
-
-        val newRatingBar = RatingBar(this, null, android.R.attr.ratingBarStyleSmall).apply {
-            rating = ratingValue
-            layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            ).apply {
-                topMargin = 8.dpToPx(context)
-            }
-        }
-
-        val newCommentText = TextView(this).apply {
-            text = commentText
-            setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
-            setTextColor(resources.getColor(R.color.black))
-            layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-        }
-
-        newCommentLayout.addView(newRatingBar)
-        newCommentLayout.addView(newCommentText)
-
-        commentsContainer.addView(newCommentLayout)
-
-        commentInput.text.clear()
-        ratingBar.rating = 0f
-    }
 
     private fun handleStudentDataException(exception: Exception) {
         exception.printStackTrace()
@@ -221,18 +212,4 @@ class ResearchDetailsActivity : AppCompatActivity() {
             Toast.LENGTH_SHORT
         ).show()
     }
-
-    private fun handleCommentPublishException(exception: Exception) {
-        exception.printStackTrace()
-        // Handle the exception according to your needs
-        Toast.makeText(this, "Error al publicar comentario.", Toast.LENGTH_SHORT).show()
-    }
-
-    // Extension function to convert DP to PX
-    private fun Int.dpToPx(context: Context): Int =
-        TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            this.toFloat(),
-            context.resources.displayMetrics
-        ).toInt()
 }
