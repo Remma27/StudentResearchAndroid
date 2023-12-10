f27a39fd972ad8c88ca84d17e627cb5699574e1b
Remma27 <emmanuelrsolano27@gmail.com>
2023-12-10 13:49:24 -0600

    rating

--- app/src/main/java/com/example/moviles1proyecto/ui/comments/MyAdapter.kt
+++ app/src/main/java/com/example/moviles1proyecto/ui/comments/MyAdapter.kt
@@ -3,16 +3,20 @@ package com.example.moviles1proyecto.ui.comments
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.moviles1proyecto.R
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MyAdapter(private val commentList: ArrayList<Comments>) :
     RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val commentText: TextView = itemView.findViewById(R.id.commentText)
-        val rating: TextView = itemView.findViewById(R.id.rating)
+        val ratingBar: RatingBar = itemView.findViewById(R.id.rating)
         val timestamp: TextView = itemView.findViewById(R.id.timestamp)
     }
 
@@ -29,12 +33,17 @@ class MyAdapter(private val commentList: ArrayList<Comments>) :
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val comment = commentList[position]
         holder.commentText.text = comment.commentText
-        holder.rating.text = comment.rating?.toString() ?: "N/A"
+        holder.ratingBar.rating =
+            (comment.rating ?: 0.0f).toFloat()  // Asegúrate de que `rating` sea de tipo Float
 
-        // Check if timestamp is not null and convert it to a readable string
-        val timestampString = comment.timestamp?.toDate()?.toString() ?: "N/A"
+        // Formatear la fecha en el formato deseado
+        val timestampString = comment.timestamp?.toDate()?.let { formatDate(it) } ?: "N/A"
         holder.timestamp.text = timestampString
     }
 
-
+    // Función para formatear la fecha
+    private fun formatDate(date: Date): String {
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        return sdf.format(date)
+    }
 }
